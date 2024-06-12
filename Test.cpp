06d5cea7622a1a569c1510240b55faad9e176969
Reveny <113244907+reveny@users.noreply.github.com>
2024-06-12 14:09:36 +0200

    Remove testing

--- Test.cpp
+++ Test.cpp
@@ -11,10 +11,6 @@
 int main() {
     printf("Hello World!\n");
 
-    uintptr_t func = HideImport::GetSymbol("libc.so", "malloc");
-    int is_hooked = IS_FUNCTION_HOOKED(func);
-    uintptr_t a = is_hooked == 1 ? NULL : func;
-
     HI_FUNCTION_POINTER(my_malloc, void*, size_t size) = HI_GET_SAFE("libc.so", "malloc");
 
     void *testMemory = my_malloc(20);
